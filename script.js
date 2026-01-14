9c8a736a1444e9a577636c5df4386c3414cc6999
amr_khaled <160667190+AmrTeemann12@users.noreply.github.com>
2026-01-15 00:43:13 +0200

    add the function for a single round
    added playRound function which check for the human choice and compare it to the random computer choice then announce the winner and add one for its score variable

--- script.js
+++ script.js
@@ -16,3 +16,64 @@ function getHumanChoice(){
     return choice.toLowerCase();
 }
 
+let playerScore = 0;
+let computerScore = 0;
+
+function playRound(human, computer){
+    switch(human){
+        case "rock":
+            switch(computer){
+                case "rock":
+                    console.log("draw")
+
+                break;
+                case "paper":
+                    console.log("You lose, paper beats rock")
+                    ++computerScore;
+                break;
+                case "scissors":
+                    console.log("You win, rock beats scissors")
+                    ++playerScore;
+                break;
+            }
+                
+        break;
+        case "paper":
+             switch(computer){
+                case "rock":
+                    console.log("You win, paper beats rock")
+                    ++playerScore;
+                break;
+                case "paper":
+                    console.log("draw")
+                break;
+                case "scissors":
+                    console.log("You lose, scissors beats paper")
+                    ++computerScore;
+                break;
+            }
+
+        break;
+        case "scissors":
+             switch(computer){
+                case "rock":
+                    console.log("You lose, rock beats scissors")
+                    ++computerScore;
+                break;
+                case "paper":
+                    console.log("You win, scissors beats paper")
+                    ++playerScore;
+                break;
+                case "scissors":
+                    console.log("draw")
+                break;
+            }
+
+        break;
+    }
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection)
